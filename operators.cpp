7e3cc68aaffa21f03fd72008b2e9601938804b2d
tilaknagat12 <95951700+tilaknagat12@users.noreply.github.com>
2021-12-13 14:51:19 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int a=6 , b=7;
+    cout<<"operators in C++:"<<endl;
+    cout<<"Following types of operators in C++"<<endl;
+    //Arthematic opetrators
+    cout<<"the value of a + b is:"<<a+b<<endl;
+    cout<<"the value of a - b is:"<<a-b<<endl;
+    cout<<"the value of a * b is:"<<a*b<<endl;
+    cout<<"the value of a / b is:"<<a/b<<endl;
+    cout<<"the value of a++ is:"<<a++<<endl;
+    cout<<"the value of ++a is:"<<++a<<endl;
+    cout<<"the value of a-- is:"<<a--<<endl;
+    cout<<"the value of --a is:"<<--a<<endl;
+
+    // Assingnment operator ---> use to assign value to variable.
+    //int a=3,b=4;
+    //char d ='T'
+
+    // Comparison operator
+    cout<<"comparison operator are:"<<endl;
+    cout<<"The value of a == b is:"<<(a==b)<<endl;
+    cout<<"The value of a > b is:"<<(a>b)<<endl;
+    cout<<"The value of a < b is:"<<(a<b)<<endl;
+    cout<<"The value of a <= b is:"<<(a<=b)<<endl;
+    cout<<"The value of a >= b is:"<<(a>=b)<<endl;
+    cout<<"The value of a != b is:"<<(a!=b)<<endl;
+
+
+
+
+
+    return(0);
+
+}
